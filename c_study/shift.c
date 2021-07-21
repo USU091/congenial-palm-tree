a822128a4b48bb0480df8d90d819f81f6293bf6d
USU091 <boneoflily@naver.com>
2021-07-21 17:44:50 +0900

    CREATE FOLDER C-LANGUAGE

---
+++ c_study/shift.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main()
+{
+
+	int a = 3;
+	int b = 0;
+
+
+	b = a |(1<<3);
+	
+	printf("%d\n", b);
+
+}
+
+	
---
+++ c_study/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+
+	char a = 24;
+	char b = 4;
+	char c = 0;
+
+
+	printf("%d\n", a<<1);
+	printf("%d\n", a>>1);
+
+}
